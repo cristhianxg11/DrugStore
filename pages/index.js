4ac255ed23ed1a189008afa9bf6c9f34aec8ab27
cristhianxg11 <cristhianxg@gmail.com>
2026-02-12 19:29:26 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,41 @@
-pages/index.js
+import { useState } from 'react'
+import { supabase } from '../lib/supabaseClient'
+import { useRouter } from 'next/router'
+
+export default function Home() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const router = useRouter()
+
+  const handleLogin = async () => {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    })
+
+    if (error) {
+      alert(error.message)
+    } else {
+      router.push('/dashboard')
+    }
+  }
+
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Almacén SaaS</h1>
+      <input
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <br /><br />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <br /><br />
+      <button onClick={handleLogin}>Ingresar</button>
+    </div>
+  )
+}
+
