7950535615fe9e749d2cd3e644e393545b49a0db
cristhianxg11 <cristhianxg@gmail.com>
2026-02-13 17:01:43 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,66 +1,67 @@
-import { useState } from 'react'
-import { supabase } from '../lib/supabaseClient'
-import { useRouter } from 'next/router'
+import { useState } from 'react';
+import { supabase } from '../lib/supabaseClient';
+import { useRouter } from 'next/router';
 import Layout from "../components/Layout";
 
 export default function Home() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const router = useRouter()
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
 
   const handleLogin = async () => {
     const { error } = await supabase.auth.signInWithPassword({
       email,
       password,
-    })
+    });
 
     if (error) {
-      alert(error.message)
+      alert(error.message);
     } else {
-      router.push('/dashboard')
+      router.push('/dashboard');
     }
-  }
+  };
 
   const handleSignup = async () => {
-  const { error } = await supabase.auth.signUp({
-    email,
-    password,
-  })
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
 
-  if (error) {
-    alert(error.message)
-  } else {
-    alert('Usuario creado correctamente')
-  }
-}
+    if (error) {
+      alert(error.message);
+    } else {
+      alert('Usuario creado correctamente');
+    }
+  };
 
   return (
-  <Layout>
-    <div style={{ padding: 40 }}>
-      <h1>Almacén SaaS</h1>
+    <Layout>
+      <div style={{ padding: 40 }}>
+        <h1>DrugStore</h1>
 
-      <input
-        placeholder="Email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
+        <input
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-      <br /><br />
+        <br /><br />
 
-      <input
-        type="password"
-        placeholder="Password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-      <br /><br />
+        <br /><br />
 
-      <button onClick={handleLogin}>Ingresar</button>
-      <button onClick={handleSignup} style={{ marginLeft: 10 }}>
-        Crear cuenta
-      </button>
-    </div>
-  </Layout>
-);
-
+        <button onClick={handleLogin}>Ingresar</button>
+        <button onClick={handleSignup} style={{ marginLeft: 10 }}>
+          Crear cuenta
+        </button>
+      </div>
+    </Layout>
+  );
 }
 
