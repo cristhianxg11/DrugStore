1529268e56d9cb4e2cc53f04cfa16f8e87c941fb
cristhianxg11 <cristhianxg@gmail.com>
2026-02-13 00:30:46 -0300

    Update products.js

--- pages/products.js
+++ pages/products.js
@@ -1,34 +1,45 @@
 import Layout from "../components/Layout";
 
-<table style={{ width: "100%", background: "white", borderRadius: "10px" }}>
-  <thead>
-    <tr style={{ background: "#f9fafb" }}>
-      <th style={th}>Nombre</th>
-      <th style={th}>Precio</th>
-      <th style={th}>Stock</th>
-    </tr>
-  </thead>
-  <tbody>
-    {products.map((product) => (
-      <tr key={product.id}>
-        <td style={td}>{product.name}</td>
-        <td style={td}>$ {product.price}</td>
-        <td style={td}>{product.stock}</td>
-      </tr>
-    ))}
-  </tbody>
-</table>
-<button
-  style={{
-    background: "#2563eb",
-    color: "white",
-    border: "none",
-    padding: "10px 16px",
-    borderRadius: "8px",
-    cursor: "pointer",
-    marginBottom: "15px"
-  }}
->
-  + Agregar Producto
-</button>
+export default function Products() {
+  const products = [];
 
+  return (
+    <Layout>
+      <h1>Productos</h1>
+
+      <button
+        style={{
+          background: "#2563eb",
+          color: "white",
+          border: "none",
+          padding: "10px 16px",
+          borderRadius: "8px",
+          cursor: "pointer",
+          marginBottom: "15px"
+        }}
+      >
+        + Agregar Producto
+      </button>
+
+      <table style={{ width: "100%", background: "white" }}>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Precio</th>
+            <th>Stock</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <tr key={product.id}>
+              <td>{product.name}</td>
+              <td>$ {product.price}</td>
+              <td>{product.stock}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+    </Layout>
+  );
+}
